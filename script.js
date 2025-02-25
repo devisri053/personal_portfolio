e0280847154cb4ae3b3e859c76846359928aff2f
Devi Srilakshmi Baddela <148530256+devisri053@users.noreply.github.com>
2025-02-25 07:46:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ toggle_box.onclick = function(){
         circle.style.transform = "translateX(50px)";
         document.body.style.backgroundColor= "rgba(199, 126, 126, 0.89)";
 <<<<<<< HEAD
-        toggle_box.style.backgroundImage = " url('https://thumbs.dreamstime.com/b/beautiful-night-sky-peaceful-background-blue-moon-falling-stars-clouds-glowing-horizon-elements-image-furnished-35701701.jpg')";
+        toggle_box.style.backgroundImage = " url('https://fixthephoto.com/images/content/moon-night-sky-png-free-81616514271.jpg')";
 =======
         toggle_box.style.backgroundImage = " url('https://static.scientificamerican.com/dam/m/57fc603972fdb/original/GettyImages-2166462891_WEB.jpg?m=1732209326.208&w=900')";
 >>>>>>> 97566d19c6697e511133d267d67a84ced7695a70
