8c1bb277a40483b1bc46008634c6948477d7a78f
devisri053 <srilakshmi.baddela@sasi.ac.in>
2025-03-02 18:49:24 +0530

    Merged remote changes

---
+++ Documents/3_month plan/node/my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Message from './Message';
+
+function App() {
+  
+  return (
+    <div>
+      <h1>This is the react</h1>
+      <Message name = "devi" age="20"/>
+    </div>
+    
+  );
+  
+}
+
+export default App;
+
+
---
+++ Documents/3_month plan/node/my-app/src/Message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Message(props){
+    return <h2>name is {props.name} , age is {props.age}</h2>;
+}
+
+export default Message;
